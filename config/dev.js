782946495fe1b38d07b21ad29d43ac83661719d5
이창호 <changhao@ichanghoui-MacBookPro.local>
2019-10-18 15:15:27 +0900

    first commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://lch2048:sook0927@cluster0-shard-00-00-vydbh.mongodb.net:27017,cluster0-shard-00-01-vydbh.mongodb.net:27017,cluster0-shard-00-02-vydbh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123'
+};
